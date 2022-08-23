bf5e9dce03c0d65fe45abd8d9153e44e84dc9ebd
Nguyễn Văn Thạnh <47846105+nguyenvanthanh312000@users.noreply.github.com>
2022-08-23 13:38:33 +0700

    Update BảoMật.cs

--- BảoMật.cs
+++ BảoMật.cs
@@ -1,28 +1,29 @@
 GMAIL ĐH nha trang : thanh.nv.60cntt@ntu.edu.vn : concat12345 
 GMAIL: thanh2000vnvn@gmail.com : 26/18maiantiem
-GMAIL thảo : thuthao1995vnvn@gmail.com : thao1995vnvn  
 FB Ngọc Triêm : 0564608286 : ngoctriem12345  : GMAIL : ngoctriem7@gmail.com : ngoctriem123
-Fb Hữu Đằng : nhoccungdau2310@gmail.com :  pass : yeuha23102000
 Fb Chính NVT : thanh2000vnvnvn@gmail.com : pass : 26/18maiantiem : GMAIL : sinhviennam2312000@gmail.com : sinhviennam2 : sdt : 0346119577
-Fb Laybon :       sacemok912@lefaqr5.com : pass : thanh12345 
+Fb Laybon :      sacemok912@lefaqr5.com : pass : thanh12345 
 FB KenBi       : 100051464960290 : covid192021vnvn
+GMAIL thảo : thuthao1995vnvn@gmail.com : thao1995vnvn  
 
-Zalo : 0346119577 : skylight312Fb 
+Zalo : 0346119577 : 2618maiantiem 
 GITHUB : nguyenvanthanh312000 : quanque12345
          vanthanh1406 : quanque12345
 Cisco packet : mail : thanh2000vnvn@gmail.com : pass : Vanthanh312Cisco packet : thanh2000vnvn@gmail.com pass: thanh-PC123
-Sinhvien.ntu.edu.vn :     60130940 : 03012000
+Sinhvien.ntu.edu.vn : 60130940 : 03012000 ( VANTHANH )
+Sinhvien.ntu.edu.vn : 60136321 : NGUYENDORAS
+Sinhvien.ntu.edu.vn : 60130217 : 21042018 
+
 TK Kali Linux :     root : 123
 Tk Ubuntu Linux :   vanthanh : thanh312
-
+Smikta.info : nguyenvanthanh312000 : thanh312000
 
 Viettinbank: 0346119577 Mk : thanh312
 Tecombank: 0346119577 Mk : Thanh312
 Techcombank : 0346119577 - Th312000
 
 Wefinex NVT : 100004612334982 : cailon31200000
-MyElet Cô Nhu B1.1: thanhnguyen312 Pass : thanhnguyen312 
-                                   Leha2310Pass : leha2310
+MyElet Cô Nhu B1.1: thanhnguyen312 Pass : thanhnguyen312 Leha2310Pass : leha2310
 MyElet A2.1:  Nguyenvanthanh312000vn : concat12345
 MyElet A2.2:  Nguyenvanthanh312000vnvn : 312000 : lethiha2310 : hamoon23102000
 Binance:  thanh2000vnvn@gmail.com : Concat12345
