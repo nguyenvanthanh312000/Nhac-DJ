4dc3143fc763c098d09406fd696370ec6b18c835
Nguyễn Văn Thạnh <47846105+nguyenvanthanh312000@users.noreply.github.com>
2022-08-23 13:22:12 +0700

    Create BảoMật.cs

---
+++ BảoMật.cs
@@ -0,0 +1,37 @@
+GMAIL ĐH nha trang : thanh.nv.60cntt@ntu.edu.vn : concat12345 
+GMAIL: thanh2000vnvn@gmail.com : 26/18maiantiem
+GMAIL thảo : thuthao1995vnvn@gmail.com : thao1995vnvn  
+FB : 0978195041 - thanh123456789
+FB Ngọc Triêm : 0564608286 : ngoctriem12345  : GMAIL : ngoctriem7@gmail.com : ngoctriem123
+Fb Hữu Đằng : nhoccungdau2310@gmail.com :  pass : yeuha23102000
+Fb Chính NVT : thanh2000vnvnvn@gmail.com : pass : 26/18maiantiem : GMAIL : sinhviennam2312000@gmail.com : sinhviennam2 : sdt : 0346119577
+Fb Laybon :       sacemok912@lefaqr5.com : pass : thanh12345 FB KenBi       : 100051464960290 : covid192021vnvn
+
+Zalo : 0346119577 : skylight312Fb Wefinex NVT : 100004612334982 : cailon312000000Thư Viện NTU :     60130940 : 03012000
+MyElet Cô Nhu B1.1: thanhnguyen312 Pass : thanhnguyen312 
+                                   Leha2310Pass : leha2310
+MyElet A2.1:  Nguyenvanthanh312000vn : concat12345
+MyElet A2.2:  Nguyenvanthanh312000vnvn : 312000 : lethiha2310 : hamoon23102000
+GITHUB : nguyenvanthanh312000 : quanque12345
+         vanthanh1406 : quanque12345
+Cisco packet : mail : thanh2000vnvn@gmail.com : pass : Vanthanh312Cisco packet : thanh2000vnvn@gmail.com             pass: thanh-PC123
+Binance:  thanh2000vnvn@gmail.com : Concat12345
+Tk : wefinex : thanh2000vnvn@gmail.com : concat12345
+Wefinex: ngoctriem7@gmail.com : MK: vanthanh312
+Tk trade: thaonguyenolly@gmail.com MK: thaonguyen
+Viettinbank: 0346119577 Mk : thanh312
+Tecombank: 0346119577 Mk : Thanh312
+Techcombank : 0346119577 - Th312000
+
+MEGACARD : TK: thuthao2000vnvn - pass: galaxy0912472663abc- MK CẤP 2: 312000
+OTT PHONG THẦN CÂN TEAM : TK: cauchufacebook312000 - MK lonlinh123456789
+Đột kích : mamay963741 mk: 78H107207
+Đổi thẻ 365 : thanh2000vnvn@gmail.com : 893484902439abcp
+Bán thẻ 24h :  thanh2000vnvn@gmail.com : 893484902439abc
+ZING ME : nguyenvanthanh0069 : 893484902439 
+Microsoft : thanh2000vnvn@gmail.com : vanvan312000    :    03012000@12345vn
+            thanh2000vsmikta : nguyenvanthanh312000   :    thanh312000
+
+
+
+
